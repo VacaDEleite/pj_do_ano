09c0a260b83fb4d19867450e9c21066e309acdee
pedro martins 2c <108538858+VacaDEleite@users.noreply.github.com>
2024-05-13 11:03:36 -0300

    s

--- main.js
+++ main.js
@@ -254,8 +254,8 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-12-05T00:00:00");
-const tempoObjetivo2 = new Date("2026-02-10T00:00:00");
-const tempoObjetivo3 = new Date("2026-02-16T00:00:00");
+const tempoObjetivo2 = new Date("2025-06-17T00:00:00");
+const tempoObjetivo3 = new Date("2026-12-12T00:00:00");
 const tempoObjetivo4 = new Date("2029-12-26T00:00:00");
 
 const tempos = [tempoObjetivo1,tempoObjetivo2,tempoObjetivo3,tempoObjetivo4];
